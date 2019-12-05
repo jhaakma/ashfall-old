1a26e0f268c803875584f73c3894b54ae0f449e4
Jeremy Haakma <jchaakma@gmail.com>
2019-12-05 16:44:57 +1300

    foodTypes and teaTypes updated by Danae

--- camping/teaTypes.lua
+++ camping/teaTypes.lua
@@ -1,29 +1,34 @@
 local this = {}
 
 this.teaTypes = {
-    ["ingred_bittergreen_petals_01"] = { description = ""},
-    ["ingred_black_anther_01"] = { description = ""},
-    ["ingred_chokeweed_01"] = { description = ""},
-    ["ingred_comberry_01"] = { description = ""},
+    ["ingred_bittergreen_petals_01"] = { description = "Bittergreen tea clarifies the mind and fortifies intelligence."},
+    ["ingred_black_anther_01"] = { description = "Drinking Black Anther tea will help you resist fire for a few hours."},
+    ["ingred_chokeweed_01"] = { description = "Chokeweed tea is well-known for protecting you from common diseases."},
+    ["ingred_comberry_01"] = { description = "Comberry tea is a favourite of Witchhunters and Crusaders for its reflective powers."},
     ["ingred_fire_petal_01"] = { description = "Fire petal tea provides a long lasting protection against the cold."},
-    ["ingred_gold_kanet_01"] = { description = ""},
-    ["ingred_gold_kanet_unique"] = { description = ""},
-    ["Ingred_golden_sedge_01"] = { description = ""},
+    -- Interestingly, the propery of fire petals is resist fire
+    ["ingred_gold_kanet_01"] = { description = "Gold Kanet tea is usually served to fortify one's strength."},
+    ["ingred_gold_kanet_unique"] = { description = "Roland's Tears tea is usually served to fortify one's strength for an exceptionnally long time."},
+    -- Or same as Gold Kanet, or stronger effect?
+    ["Ingred_golden_sedge_01"] = { description = "Golden Sage tea is a favourite among fighters as it fortifies their attack for a while."},
     ["ingred_hackle-lo_leaf_01"] = { description = "Hackle-lo tea can stave off the need for sleep for a few hours."},
-    ["ingred_heather_01"] = { description = ""},
-    ["ingred_holly_01"] = { description = ""},
-    ["ingred_kresh_fiber_01"] = { description = ""},
+    ["ingred_heather_01"] = { description = "While Heather tea has a peculiar taste, it also fortifies personality."},
+    ["ingred_holly_01"] = { description = "Holly Berry tea is often served in Solstheim for its long lasting protection against the cold."},
+    ["ingred_kresh_fiber_01"] = { description = "Ashlanders believe Kresh Fiber tea brings luck."},
     ["Ingred_lloramor_spines_01"] = { description = ""},
-    ["Ingred_meadow_rye_01"] = { description = ""},
+    -- spell absorbtion
+    ["Ingred_meadow_rye_01"] = { description = "Meadow Rye tea is favoured by Thieves and those generally pressed for time."},
     ["Ingred_nirthfly_stalks_01"] = { description = ""},
-    ["Ingred_noble_sedge_01"] = { description = ""},
-    ["ingred_roobrush_01"] = { description = ""},
+    ["Ingred_noble_sedge_01"] = { description = "A well-known beverage among Acrobats, the Noble Sledge tea improves one's Agility."},
+    ["ingred_roobrush_01"] = { description = "Many find Roobrush tea too fragrant but still drink it for its protection against poison."},
     ["ingred_bc_spore_pod"] = { description = ""},
+    -- has no beneficial effect apart fron detect key.
     ["ingred_stoneflower_petals_01"] = { description = "Drinking stoneflower tea will make you immune to paralysis for a few hours."},
-    ["Ingred_timsa-come-by_01"] = { description = ""},
+    ["Ingred_timsa-come-by_01"] = { description = "For better or for worse, Timsa-Come-By tea grants temporary resitance to magicka."},
     ["ingred_treated_bittergreen_uniq"] = { description = ""},
-    ["ingred_belladonna_01"] = { description = ""},
-    ["ingred_belladonna_02"] = { description = ""},
-    ["ingred_wickwheat_01"] = { description = ""},
+    -- prolly best to not use this?
+    ["ingred_belladonna_01"] = { description = "Belladonna tea is rare and very sought of as it restores magicka for a while."},
+    ["ingred_belladonna_02"] = { description = "Belladonna tea made from unrippended berries will restore magicka for an exceptionnally long time"},
+    ["ingred_wickwheat_01"] = { description = "Wickwheat tea is commonly used and is beleived to fortify health."},
 }
 return this
