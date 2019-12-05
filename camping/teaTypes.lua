d1f83b29edc25dc97b9265647903ce8d461f6caa
Jeremy Haakma <jchaakma@gmail.com>
2019-12-05 18:31:44 +1300

    udpating teaType descriptions

--- camping/teaTypes.lua
+++ camping/teaTypes.lua
@@ -1,34 +1,85 @@
 local this = {}
 
 this.teaTypes = {
-    ["ingred_bittergreen_petals_01"] = { description = "Bittergreen tea clarifies the mind and fortifies intelligence."},
-    ["ingred_black_anther_01"] = { description = "Drinking Black Anther tea will help you resist fire for a few hours."},
-    ["ingred_chokeweed_01"] = { description = "Chokeweed tea is well-known for protecting you from common diseases."},
-    ["ingred_comberry_01"] = { description = "Comberry tea is a favourite of Witchhunters and Crusaders for its reflective powers."},
-    ["ingred_fire_petal_01"] = { description = "Fire petal tea provides a long lasting protection against the cold."},
+    ["ingred_bittergreen_petals_01"] = { 
+        teaDescription = "The overbearing aroma of Bittergreen tea helps to cleanse the mind of distracting thoughts.",
+        effectDescription = "Fortify Magicka 15 points"
+    },
+    ["ingred_black_anther_01"] = { 
+        teaDescription = "A popular drink among socialites and those who wish to stand out, Black Anther tea gives the skin a healthy, radiant glow.",
+        effectDescription = "Light 3 points on Self"
+    },
+    ["ingred_chokeweed_01"] = { 
+        teaDescription = "Drinking Chokeweed tea helps boost your immune system. Like any good medicine, it taste absolutely terrible.",
+        effectDescription = "Resist Common Disease 25 points"
+    },
+    ["ingred_comberry_01"] = { 
+        teaDescription = "Witchhunters and Crusaders are known to drink this tea before battle to help fend off magical attacks.",
+        effectDescription = "Reflect 5 points"
+    },
+    ["ingred_fire_petal_01"] = {
+        teaDescription = "Fire Petal tea is a spicy beverage that helps keep one warm on cold nights.",
+        effectDescription = "Reduce cold weather effects by 15%"
+    },
     -- Interestingly, the propery of fire petals is resist fire
-    ["ingred_gold_kanet_01"] = { description = "Gold Kanet tea is usually served to fortify one's strength."},
-    ["ingred_gold_kanet_unique"] = { description = "Roland's Tears tea is usually served to fortify one's strength for an exceptionnally long time."},
-    -- Or same as Gold Kanet, or stronger effect?
-    ["Ingred_golden_sedge_01"] = { description = "Golden Sage tea is a favourite among fighters as it fortifies their attack for a while."},
-    ["ingred_hackle-lo_leaf_01"] = { description = "Hackle-lo tea can stave off the need for sleep for a few hours."},
-    ["ingred_heather_01"] = { description = "While Heather tea has a peculiar taste, it also fortifies personality."},
-    ["ingred_holly_01"] = { description = "Holly Berry tea is often served in Solstheim for its long lasting protection against the cold."},
-    ["ingred_kresh_fiber_01"] = { description = "Ashlanders believe Kresh Fiber tea brings luck."},
-    ["Ingred_lloramor_spines_01"] = { description = ""},
-    -- spell absorbtion
-    ["Ingred_meadow_rye_01"] = { description = "Meadow Rye tea is favoured by Thieves and those generally pressed for time."},
-    ["Ingred_nirthfly_stalks_01"] = { description = ""},
-    ["Ingred_noble_sedge_01"] = { description = "A well-known beverage among Acrobats, the Noble Sledge tea improves one's Agility."},
-    ["ingred_roobrush_01"] = { description = "Many find Roobrush tea too fragrant but still drink it for its protection against poison."},
-    ["ingred_bc_spore_pod"] = { description = ""},
+    ["ingred_gold_kanet_01"] = { 
+        teaDescription = "Gold Kanet tea is usually served to fortify one's strength.",
+        effectDescription = "Fortify Strength 5 points"
+    },
+    ["Ingred_golden_sedge_01"] = { 
+        teaDescription = "A favourite among fighters, Golden Sage tea increases attack power.",
+        effectDescription = ""
+    },
+    ["ingred_hackle-lo_leaf_01"] = { 
+        teaDescription = "Hackle-lo tea increases energy and alertness, allowing one to stay awake for longer.",
+        effectDescription = ""
+    },
+    ["ingred_heather_01"] = { 
+        teaDescription = "Heather tea is a relaxing beverage that makes the weight on your shoulders feel a litte lighter.",
+        effectDescription = "Feather 10 points"
+    },
+    ["ingred_holly_01"] = { 
+        teaDescription = "A sweet, fragrant tea often served in Solstheim for its ability to stave off the cold.",
+        effectDescription = "Reduce cold weather effects by 10%"
+    },
+    ["ingred_kresh_fiber_01"] = { 
+        teaDescription = "Ashlanders believe that drinking tea brewed from Kresh Fiber brings good fortune.",
+        effectDescription = "Fortify Luck 5 points."
+    },
+    ["Ingred_meadow_rye_01"] = { 
+        teaDescription = "Drinking this tea causes the mind to buzz and the limbs to fidget about.",
+        effectDescription = "Fortify Speed 5 points"
+    },
+    ["Ingred_noble_sedge_01"] = { 
+        teaDescription = "A rare beverage, prized among Acrobats, the Noble Sledge tea improves one's Agility.",
+        effectDescription = "Fortify Agility 10 points"
+    },
+    ["ingred_roobrush_01"] = { 
+        teaDescription = "Roobrush tea has a smooth, slightly nutty flavor. Drinking it helps steady the hands, allowing for more precise movements.",
+        effectDescription = "Fortify Agility 5 points"
+    },
+
+    ["ingred_bc_coda_flower"] = {
+        teaDescription = "Tea made from the coda flower produces a mild psychotropic effect: a feeling of transcendanc ",
+        effectDescription = "Detect Animal 20 points"
+    },
+    
     -- has no beneficial effect apart fron detect key.
-    ["ingred_stoneflower_petals_01"] = { description = "Drinking stoneflower tea will make you immune to paralysis for a few hours."},
-    ["Ingred_timsa-come-by_01"] = { description = "For better or for worse, Timsa-Come-By tea grants temporary resitance to magicka."},
-    ["ingred_treated_bittergreen_uniq"] = { description = ""},
-    -- prolly best to not use this?
-    ["ingred_belladonna_01"] = { description = "Belladonna tea is rare and very sought of as it restores magicka for a while."},
-    ["ingred_belladonna_02"] = { description = "Belladonna tea made from unrippended berries will restore magicka for an exceptionnally long time"},
-    ["ingred_wickwheat_01"] = { description = "Wickwheat tea is commonly used and is beleived to fortify health."},
+    ["ingred_stoneflower_petals_01"] = { 
+        teaDescription = "The pleasant, floral aroma of Stoneflower tea lingers on the breath longer after it is consumed.",
+        effectDescription = "Fortify Speechcraft 10 points"
+    },
+    ["Ingred_timsa-come-by_01"] = { 
+        teaDescription = "Tea brewed from this rare plant makes one highly resistant to paralysis.",
+        effectDescription = "Resis Paralysis 40 points"
+    },
+    ["ingred_belladonna_01"] = { 
+        teaDescription = "Belladonna berries make for a slighty bitter tea that provides a mild resistance against magicka.",
+        effectDescription = "Resist Magicka 10 points"
+    },
+    ["ingred_belladonna_02"] = { 
+        teaDescription = "Belladonna berries make for a slighty bitter tea that provides a mild resistance against magicka.",
+        effectDescription = "Resist Magicka 10 points"
+    },
 }
 return this
